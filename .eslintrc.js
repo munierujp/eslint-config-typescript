3ba875c4cd9f08c92cb3a97d3d138c00986eab07
Munieru <20086673+munierujp@users.noreply.github.com>
2024-04-07 10:00:38 +0900

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+// @ts-check
+
+// eslint-disable-next-line jsdoc/valid-types -- https://www.typescriptlang.org/docs/handbook/jsdoc-supported-types.html
+/** @typedef {import('eslint').ESLint.ConfigData} ConfigData */
+
+/**
+ * @type {ConfigData}
+ * @see https://eslint.org/docs/latest/use/configure/configuration-files
+ */
+const config = {
+  extends: './index.js',
+  overrides: [
+    // https://typescript-eslint.io/troubleshooting/#i-get-errors-telling-me-eslint-was-configured-to-run--however-that-tsconfig-does-not--none-of-those-tsconfigs-include-this-file
+    {
+      files: ['./**/*.js'],
+      extends: ['plugin:@typescript-eslint/disable-type-checked']
+    }
+  ]
+}
+
+// eslint-disable-next-line unicorn/prefer-module
+module.exports = config
